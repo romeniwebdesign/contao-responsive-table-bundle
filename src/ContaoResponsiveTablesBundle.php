b26fad06145322e3798bded7691bf07f07945d2a
Christian Romeni <christian@romeni.eu>
2021-01-09 15:19:40 +0000

    fixed another typo and namings

--- src/ContaoResponsiveTablesBundle.php
+++ src/ContaoResponsiveTablesBundle.php
@@ -10,10 +10,10 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
-namespace Contao\SkeletonBundle;
+namespace Rwd\ContaoResponsiveTablesBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class ContaoSkeletonBundle extends Bundle
+class ContaoResponsiveTablesBundle extends Bundle
 {
 }
--- src/DependencyInjection/ContaoResponsiveTablesBundle.php
+++ src/DependencyInjection/ContaoResponsiveTablesBundle.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
-namespace namespace Rwd\ContaoResponsiveTablesBundle\DependencyInjection;
+namespace Rwd\ContaoResponsiveTablesBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
